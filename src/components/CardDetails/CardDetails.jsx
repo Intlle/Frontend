9631390fafccc235533b91c48c8b1ff037646b08
kk10 <kamy.zero.10@gmail.com>
2024-03-20 19:34:41 +0300

    Some changes

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -15,8 +15,11 @@ function CardDetails() {
             <div className={styles.box}>
                 <button className={styles.hard}>Тяжело-о-о...</button>
                 <div className={styles.card}>
-                    <h1 className={styles.title} >Кибернетика</h1>
-                    <p className={styles.description}>Наука об управлении сложными системами с обратной связью. <br/><br/> Термин "Кибернетика" впервые появился в одном из сочинений Платона, заявлявшего о том, что всякое хорошее государство должно уметь управлять вообще, а не только разбираться в непосредственно</p>
+                    <div>
+                        <h1 className={styles.title} >Кибернетика</h1>
+                        <p className={styles.description}>Наука об управлении сложными системами с обратной связью. <br/><br/> Термин "Кибернетика" впервые появился в одном из сочинений Платона, заявлявшего о том, что всякое хорошее государство должно уметь управлять вообще, а не только разбираться в непосредственно</p>
+                    </div>
+                    <button className={styles.turn}>-----------</button>
                 </div>
                 <div className={styles.twobtn}>
                     <button className={styles.good}>Получилось вспомнить</button>
